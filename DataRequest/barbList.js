806d7b676cc410f223b32b2cb16bb787d55956ba
Luke <blackcobra00@hotmail.com>
2022-08-19 22:34:27 -0400

    Updated BarbShaper and ScoutSend

--- DataRequest/barbList.js
+++ DataRequest/barbList.js
@@ -3,19 +3,19 @@ fs = require('fs');
 const contents = fs.readFileSync('/Users/Black/Code/Tribalwars/tw/gamedata/village129.txt', 'utf-8');
 const arr = contents.split(/\r?\n/);
 
-let json = {}
+let ar = [];
 
 for(const line of arr) {
     data = line.split(",");
     id = data[0];
     coord = data[2] + "|" + data[3];
 
-    if(data[4] == "0") json[coord] = id;
+    if(data[4] == "0") ar.push(coord);
 }
 
-let output = JSON.stringify(json);
+let output = JSON.stringify(ar);
 
-fs.writeFile('/Users/Black/Code/Tribalwars/tw/gamedata/village129coordID.json', output, err => {
+fs.writeFile('/Users/Black/Code/Tribalwars/tw/gamedata/village129barbs.json', output, err => {
     if (err) {
       console.error(err);
     }
