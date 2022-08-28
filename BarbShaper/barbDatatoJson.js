7ff8f32664380942b3e1275a0dbf9c250da5e03d
Luke <blackcobra00@hotmail.com>
2022-08-28 14:30:22 -0400

    Updated BarbShaper to autoimports

---
+++ BarbShaper/barbDatatoJson.js
@@ -0,0 +1,37 @@
+fs = require('fs');
+var villageIDs = require('../gamedata/village129IDcoord.json')
+
+const contents = fs.readFileSync('/Users/Black/Code/Tribalwars/tw/BarbShaper/barbData.txt', 'utf-8');
+const arr = contents.split(/\r?\n/).splice(1);
+
+const buildings = ["main", "barracks", "stable", "garage", "watchtower", "snob", "smith", "place", "statue", "market", "wood", "stone", "iron", "farm", "storage", "hide", "wall"]
+let obj = [];
+
+for(const line of arr) {
+    tempObj = {};
+
+    data = line.split(",");
+    id = data[0];
+    target = villageIDs[id];
+
+    tempObj["ID"] = id;
+    tempObj["target"] = target;
+
+    for(let i = 1; i < data.length; i++){
+        if(i == 9) {
+          continue;
+        }
+        tempObj[buildings[i - 1]] = data[i];
+    }
+
+    obj.push(tempObj)
+}
+
+let output = JSON.stringify(obj);
+
+fs.writeFile('/Users/Black/Code/Tribalwars/tw/BarbShaper/barbData.json', output, err => {
+    if (err) {
+      console.error(err);
+    }
+    // file written successfully
+  });
