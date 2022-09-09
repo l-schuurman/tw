9430b3ea1d44efdc2a10ae78d65ed026cd35d4e0
Luke <blackcobra00@hotmail.com>
2022-09-09 13:50:43 -0400

    Updated ScoutSend

--- ScoutSend/scoutImports.js
+++ ScoutSend/scoutImports.js
@@ -1,4 +1,4 @@
-import spyReports from '../gamedata/spyReports.json' assert {type: 'json'};
+import spyReports from '../gamedata/spyReportsRR.json' assert {type: 'json'};
 import villageIDs from '../gamedata/village129coordID.json' assert {type: 'json'};
 import barbs from '../gamedata/village129barbs.json' assert {type: 'json'};
 window.spyReports = spyReports;
