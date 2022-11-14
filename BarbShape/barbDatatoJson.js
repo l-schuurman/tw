1f89b20e18d4b1b35ea7d11f03a865dd696dc00f
Luke <blackcobra00@hotmail.com>
2022-11-14 16:55:02 -0500

    Updated 131 scripts

--- BarbShape/barbDatatoJson.js
+++ BarbShape/barbDatatoJson.js
@@ -1,5 +1,5 @@
 fs = require('fs');
-var villageIDs = require('../gamedata/village129IDcoord.json')
+var villageIDs = require('../gamedata/village131IDcoord.json')
 
 const contents = fs.readFileSync('/Users/Black/Code/Tribalwars/tw/BarbShape/barbData.txt', 'utf-8');
 const arr = contents.split(/\r?\n/).splice(1);
