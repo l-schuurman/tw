e7e68941c777382987df1e21ea00efea32d54143
Luke Schuurman <schuurmL@mcmaster.ca>
2023-06-26 10:14:16 -0400

    Update ScoutSend gamedata links

--- ScoutSend/scoutImports.js
+++ ScoutSend/scoutImports.js
@@ -1,6 +1,6 @@
-import spyReports from '../gamedata/spyReportsRR.json' assert {type: 'json'};
-import villageIDs from '../gamedata/village129coordID.json' assert {type: 'json'};
-import barbs from '../gamedata/village129barbs.json' assert {type: 'json'};
+import spyReports from '../gamedata/spyReportsWJF.json' assert {type: 'json'};
+import villageIDs from '../gamedata/village133coordID.json' assert {type: 'json'};
+import barbs from '../gamedata/village133barbs.json' assert {type: 'json'};
 window.spyReports = spyReports;
 window.villageIDs = villageIDs;
 window.barbs = barbs;
